acd5108ff16a567315d6abe3822ab97839b6d67e
adria <adria@WassPC>
2020-11-30 21:03:55 +0100

    Creación de las clases iniciales.

--- src/arrays_cc/CadenaCaracteres.java
+++ src/arrays_cc/CadenaCaracteres.java
@@ -15,21 +15,53 @@ import java.util.*;
  */
 public class CadenaCaracteres {
     
-    public static int opcionesMenu(int opcion){
+    public static void opcionesMenu(){
+        
+        // DECLARACIÓN DE VARIABLES
+        
+        int opcion = 0;
+        
+        // Colores para la consola
+        
+        String rojo="\033[31m";
+        String reset="\u001B[0m";
         
         Scanner teclado = new Scanner (System.in);
         
+        // PROCESAMIENTO
+        
         do {
             
-            System.out.println("Bienvenido/a al menú de ejercicios con cadenas de"
-                    + "caracteres.");
+            System.out.println("\nBienvenido/a al menú de ejercicios con cadenas de"
+                    + " caracteres.");
             System.out.println("---------------------------------------------");
-            System.out.println("1. Buscar una palabra en el texto y verla por pantalla.");
-            System.out.println("2. Sustituir una palabra por otra.");
+            System.out.println("1. Opción 1");
+            System.out.println("2. Opción 2");
+            
+            System.out.println("\nSelecciona una de las opciones: ");
+            String opc = teclado.nextLine() ;
+            try
+            {
+                opcion = Integer.parseInt(opc);
+            }
+            catch (NumberFormatException e){
+                
+                System.out.println(rojo +  "Introduce un número válido, por favor.\n" + reset);
+            }
+            catch (Exception e){
+                
+                System.out.println(rojo + "Puede que hayas introducido algo mal. \n" + reset);
+            }
+            
             
         } while (opcion!=0);
         
-        
+        System.out.println("--------------------------------------------");
+        System.out.println("Has salido del menú. \n");
     }
-    
+    public static void main(String[] args) { //MAIN PROVISIONAL
+        
+        opcionesMenu();
+    }
+   
 }
