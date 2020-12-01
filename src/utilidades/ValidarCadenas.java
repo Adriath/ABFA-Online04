901bdf5f1dd999a3521afeca809ebcac1a0ca507
adria <adria@WassPC>
2020-12-01 17:28:55 +0100

    Terminado el método compruebaMatriculaAutomovil.

--- src/utilidades/ValidarCadenas.java
+++ src/utilidades/ValidarCadenas.java
@@ -9,17 +9,25 @@ import java.util.*;
 import java.util.regex.*;
 
 /**
- *
+ * 
  * @author Adrián Arjona
  * @version 1.0
  */
 public class ValidarCadenas {
+   
+    /**
+     * Comprueba la matrícula (String) introcida por parámetro y la compara con el 
+     * modelo registrado en el validador para las matrículas de automóvil.
+     * 
+     * @param matricula Cadena de caracteres tipo String que se comparará con el patrón.
+     * @return booleano, true si es válido, false si no es válido.
+     */
     
     public static boolean comprobarMatriculaAutomovil(String matricula){
         
-        boolean valido = false;
+        boolean valido;
         
-        Pattern p = Pattern.compile("prueba");
+        Pattern p = Pattern.compile("^[\\d]{4}+[A-Z&&[^ÑAEIOU]]{3}$"); // Formato matrícula: 0000AAA (No vocales ni ñ)
         Matcher m = p.matcher(matricula);
         
         if (m.matches()) {
@@ -30,9 +38,6 @@ public class ValidarCadenas {
         
         return valido;
         
-    } 
-    
-    public static void main(String[] args) {
-        System.out.println(comprobarMatriculaAutomovil("prueba"));
     }
+    
 }
