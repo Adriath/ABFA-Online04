3a8d654618e0e57e56a9d71a53f1fd4a9aedec1c
adria <adria@WassPC>
2020-12-02 21:55:34 +0100

    Clase HundirFlota. He conseguido hacer el tablero con letras y todo. -- He realizado cambios menores

--- src/hundir_la_flota/HundirFlota.java
+++ src/hundir_la_flota/HundirFlota.java
@@ -5,6 +5,8 @@
  */
 package hundir_la_flota;
 
+import utilidades.IO_ES;
+
 /**
  * Juego de Hundir la Flota.
  * 
@@ -13,7 +15,13 @@ package hundir_la_flota;
  */
 public class HundirFlota {
     
-    public static void tablero(){
+    final int FILA = 11 ;
+    final int COLUMNA = 11 ;
+    char letras = 'A' ;
+
+    String[][] tablero = new String[COLUMNA][FILA] ;
+    
+    public static void tablero(boolean visualizar){ //true para visualizar, false para ocultar
         
         // ------------ DECLARACIONES ------------
         
@@ -41,33 +49,33 @@ public class HundirFlota {
         
         for (int i = 1; i < COLUMNA; i++)
         {
-            tablero[i][0] = Character.toString(letras);
+            tablero[i][0] = "[" + Character.toString(letras) + "]";
             letras++;
             
             for (int j = 1; j < FILA; j++)
             {
-                tablero[0][j] = Integer.toString(j - 1) + "  ";
+                tablero[0][j] = "[" + (Integer.toString(j)) + "]";
             }
         }
         
         // Visualizar tablero
         
-        for (int i = 0; i < COLUMNA; i++)
+        if (visualizar == true)
         {
+            for (int i = 0; i < COLUMNA; i++)
+            {
             System.out.println("\n");
             
-            for (int j = 0; j < FILA; j++)
-            {
-                System.out.print(tablero[i][j] + "   ");
+                for (int j = 0; j < FILA; j++)
+                {
+                    System.out.print(tablero[i][j] + "     ");
+                }
             }
+            
         }
         
     }
-    public static void main(String[] args) {
-        
-        tablero();
-        
-        
-        // la a es 97. ¿Y la A mayúscula? 65
+        public static void main(String[] args) {
+            tablero(false);
     }
 }
